f75796f4fd2a84dbb9af58e8372e03906ca6d7f5
Jimnezoh <jimmaina100@gmail.com>
2025-06-19 02:57:43 +0300

    established an api connection to get a token via the login route

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.models import User
 
 # Create your views here.
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls,user):
+        token = super().get_token(user)
+        token['role'] = user.role
+        token['full_name'] = user.full_name
+        token['phone_number'] = user.phone_number
+        return token
+    
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
