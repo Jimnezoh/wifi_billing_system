a137f09d3215dfbad7f6b081aaa6cf1ee7d45394
Jimnezoh <jimmaina100@gmail.com>
2025-06-20 23:57:20 +0300

    introduced serielizers.py to post data into the api and facilitate ccmmunication with the accesscopde model

---
+++ wifi/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import WifiAccessCode
+from users.models import User
+
+class WifiAccessCodeSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(write_only=True)
+    full_name = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = WifiAccessCode
+        fields = ['phone_number', 'full_name', 'amount_paid', 'duration_minutes', 'code', 'expires_at']
+        read_only_fields = ['code', 'expires_at']
+
+    def create(self, validated_data):
+        phone = validated_data.pop('phone_number')
+        name = validated_data.pop('full_name', None)
+
+        # Normalize phone
+        phone = phone.strip().replace(" ", "").replace("+", "")
+        if phone.startswith("07") and len(phone) == 10:
+            phone = "254" + phone[1:]
+
+        # Get or create the user
+        user, created = User.objects.get_or_create(phone_number=phone, defaults={"full_name": name, "role": "client"})
+
+        access_code = WifiAccessCode.objects.create(user=user, **validated_data)
+        return access_code
